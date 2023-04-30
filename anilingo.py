0766bbcdc758408d58dceb0653207e065dfe7cd9
mahlernim <33252024+mahlernim@users.noreply.github.com>
2023-04-30 16:35:32 +0900

    First working prototype

---
+++ anilingo.py
@@ -0,0 +1,145 @@
+import pygame
+import yaml
+import random
+
+with open('lyrics.yaml', 'r', encoding="utf-8") as file:
+    lyrics_data = yaml.safe_load(file)
+
+random_lyric = random.choice(lyrics_data)
+original_lyric = random_lyric['original']
+english_translation = random_lyric['english']
+korean_translation = random_lyric['korean']
+original_tokens = random_lyric['tokens'].split(',')
+tokens = original_tokens.copy()
+random.shuffle(tokens)
+
+pygame.init()
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Ani-Lingo")
+font_path = "Noto Sans Mono CJK JP Regular.otf"
+font_size = 32
+font = pygame.font.Font(font_path, font_size)
+font_path = "gulim.ttc"
+font_size = 32
+kfont = pygame.font.Font(font_path, font_size)
+box_width, box_height = 120, 60
+box_margin = 20
+box_color = {"default": (200, 200, 200), "selected": (230, 230, 230)}
+selected_tokens = []
+
+def create_button(text, x, y, width, height):
+    button_rect = pygame.Rect(x, y, width, height)
+    button_text = font.render(text, True, (0, 0, 0))
+    button_text_rect = button_text.get_rect(center=button_rect.center)
+    return button_rect, button_text, button_text_rect
+
+def draw_button(button_rect, button_text, button_text_rect):
+    pygame.draw.rect(screen, (200, 200, 200), button_rect, 0)
+    screen.blit(button_text, button_text_rect)
+
+def reset_game():
+    global random_lyric, original_lyric, english_translation, korean_translation, tokens, selected_tokens
+    random_lyric = random.choice(lyrics_data)
+    original_lyric = random_lyric['original']
+    english_translation = random_lyric['english']
+    korean_translation = random_lyric['korean']
+    tokens = random_lyric['tokens'].split(',')
+    random.shuffle(tokens)
+    selected_tokens = []
+
+def draw_token_boxes(tokens):
+    global start_x, y
+    num_tokens = len(tokens)
+    max_line_width = screen_width - 2 * box_margin
+    num_tokens_per_line = max_line_width // (box_width + box_margin)
+    line_width = num_tokens_per_line * box_width + (num_tokens_per_line - 1) * box_margin
+    num_lines = (num_tokens + num_tokens_per_line - 1) // num_tokens_per_line
+    start_y = (screen_height - (num_lines * (box_height + box_margin) - box_margin)) // 2
+    token_index = 0
+    token_boxes = []
+    global_index = 0
+    for line in range(num_lines):
+        line_tokens = tokens[token_index:token_index + num_tokens_per_line]
+        num_tokens_in_line = len(line_tokens)
+        line_width = num_tokens_in_line * box_width + (num_tokens_in_line - 1) * box_margin
+        start_x = (screen_width - line_width) // 2
+        y = start_y + (box_height + box_margin) * line
+
+        for idx, token in enumerate(line_tokens):
+            global_index = idx + line * num_tokens_per_line
+            box_x = start_x
+            box_rect = pygame.Rect(box_x, y, box_width, box_height)
+
+            if global_index in selected_tokens:
+                box_color_key = "selected"
+            else:
+                box_color_key = "default"
+
+            pygame.draw.rect(screen, box_color[box_color_key], box_rect, 0)
+            text = font.render(token, True, (0, 0, 0))
+            text_rect = text.get_rect(center=(box_rect.centerx, box_rect.centery))
+            screen.blit(text, text_rect)
+            start_x += box_width + box_margin
+            global_index += 1
+            token_boxes.append(box_rect)
+        token_index += num_tokens_per_line
+    pygame.display.flip()
+    return token_boxes
+
+running = True
+needs_redraw = True
+next_question_button, next_question_text, next_question_text_rect = create_button("Next question", 200, 500, 200, 50)
+try_again_button, try_again_text, try_again_text_rect = create_button("Try again", 450, 500, 200, 50)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for i, box_rect in enumerate(token_boxes):
+                if box_rect.collidepoint(event.pos):
+                    if i in selected_tokens:
+                        selected_tokens.remove(i)  # Deselect the token
+                    else:
+                        selected_tokens.append(i)  # Select the token
+                    needs_redraw = True
+                    break
+
+    if needs_redraw:
+        screen.fill((255, 255, 255))
+        english_text = font.render(english_translation, True, (0, 0, 0))
+        english_text_rect = english_text.get_rect(center=(screen_width // 2, 50))
+        screen.blit(english_text, english_text_rect)
+        korean_text = kfont.render(korean_translation, True, (0, 0, 0))
+        korean_text_rect = korean_text.get_rect(center=(screen_width // 2, 80))
+        screen.blit(korean_text, korean_text_rect)
+        token_boxes = draw_token_boxes(tokens)
+        selected_text = " ".join(tokens[i] for i in selected_tokens)
+        selected_text_surface = font.render(selected_text, True, (0, 0, 0))
+        selected_text_rect = selected_text_surface.get_rect(center=(screen_width // 2, 200))
+        screen.blit(selected_text_surface, selected_text_rect)
+
+        if len(selected_tokens) == len(tokens):
+            selected_token_values = [tokens[i] for i in selected_tokens]
+            #print(selected_token_values, original_tokens)
+            if selected_token_values == original_tokens:
+                result_text = "Correct!"
+                draw_button(next_question_button, next_question_text, next_question_text_rect)
+            else:
+                result_text = "Incorrect..."
+                draw_button(try_again_button, try_again_text, try_again_text_rect)
+            result_text_surface = font.render(result_text, True, (0, 0, 0))
+            result_text_rect = result_text_surface.get_rect(center=(screen_width // 2, 150))
+            screen.blit(result_text_surface, result_text_rect)
+        pygame.display.flip()
+        needs_redraw = False
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if next_question_button.collidepoint(event.pos) and len(selected_tokens) == len(tokens) and selected_token_values == original_tokens:
+            reset_game()
+            needs_redraw = True
+        elif try_again_button.collidepoint(event.pos) and len(selected_tokens) == len(tokens) and selected_tokens != tokens:
+            selected_tokens = []
+            needs_redraw = True
+pygame.quit()
